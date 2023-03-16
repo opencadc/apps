f42fd75bc2c1dbf2b9abe23f30583f23d93ec9db
Dustin Jenkins <djenkins.cadc@gmail.com>
2023-03-16 14:20:53 -0700

    Review rework, and shrink overall script size for authenticated access.

--- cadc-download-manager-server/src/test/resources/cadc-download-expected-auth.sh
+++ cadc-download-manager-server/src/test/resources/cadc-download-expected-auth.sh
@@ -2,14 +2,16 @@
 
 echo "Warning!  The token provided in the cURL commands will expire on 1977-11-25T01:15:00."
 
+TOKEN="SUPERTOKEN"
+
 echo "https://mysite.com/download/proprietary-1.fits"
-curl --location --remote-name --remote-header-name --progress-bar -H "authorization: bearer SUPERTOKEN" "https://mysite.com/download/proprietary-1.fits"
+curl --location --remote-name --remote-header-name --progress-bar -H "authorization: bearer ${TOKEN}" "https://mysite.com/download/proprietary-1.fits"
 
 echo "https://mysite.com/download/proprietary-2.fits"
-curl --location --remote-name --remote-header-name --progress-bar -H "authorization: bearer SUPERTOKEN" "https://mysite.com/download/proprietary-2.fits"
+curl --location --remote-name --remote-header-name --progress-bar -H "authorization: bearer ${TOKEN}" "https://mysite.com/download/proprietary-2.fits"
 
 echo "https://mysite.com/download/proprietary-3.fits"
-curl --location --remote-name --remote-header-name --progress-bar -H "authorization: bearer SUPERTOKEN" "https://mysite.com/download/proprietary-3.fits"
+curl --location --remote-name --remote-header-name --progress-bar -H "authorization: bearer ${TOKEN}" "https://mysite.com/download/proprietary-3.fits"
 
 echo "https://mysite.com/download/proprietary-4.fits"
-curl --location --remote-name --remote-header-name --progress-bar -H "authorization: bearer SUPERTOKEN" "https://mysite.com/download/proprietary-4.fits"
+curl --location --remote-name --remote-header-name --progress-bar -H "authorization: bearer ${TOKEN}" "https://mysite.com/download/proprietary-4.fits"
